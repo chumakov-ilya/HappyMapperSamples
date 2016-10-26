cb4d801c20da1883da954ae0eb7819bcbe7ba4cd
chumakov-ilya <aidforwork@gmail.com>
2016-10-26 18:36:37 +0300

    Fix "insufficient memory" issue and add approx. results set.

--- HighloadTest/Highload_SmokeTests.cs
+++ HighloadTest/Highload_SmokeTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using NUnit.Framework;
 
 namespace HighloadTest
 {
@@ -16,6 +19,33 @@ namespace HighloadTest
             var mapper = MapContainer.CreateMaps();
 
             MapContainer.Map(mapper);
+            GC.Collect();
+            Thread.Sleep(TimeSpan.FromSeconds(30));
+            GC.Collect();
+            Trace.WriteLine($"GC.GetTotalMemory(true): {GC.GetTotalMemory(true).FromBytesToMegabytes()} MB");
+            Trace.WriteLine($"Process.GetCurrentProcess().WorkingSet64: {Process.GetCurrentProcess().WorkingSet64.FromBytesToMegabytes()} MB");
+
+            //Approximate results per pair count (x64 process):
+
+            //100
+            //GC.GetTotalMemory(true): 11
+            //Process.GetCurrentProcess().WorkingSet64: 74
+
+            //200
+            //GC.GetTotalMemory(true): 15
+            //Process.GetCurrentProcess().WorkingSet64: 81
+
+            //1000
+            //GC.GetTotalMemory(true): 44
+            //Process.GetCurrentProcess().WorkingSet64: 142
+        }
+    }
+
+    public static class LongExtentions
+    {
+        public static long FromBytesToMegabytes(this long value)
+        {
+            return (long)(value/Math.Pow(2, 20));
         }
     }
 }
